35bc42aeaef08cadd41e13cc76938bbb4807a405
Shri-2512 <shrinivas.chavan0987@gmail.com>
2024-10-18 03:06:13 +0530

    commit no 13

--- app/src/main/java/com/example/suraksha/MainActivity.java
+++ app/src/main/java/com/example/suraksha/MainActivity.java
@@ -1,10 +1,7 @@
 package com.example.suraksha;
-import com.example.suraksha.DatabaseHelper;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,38 +18,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         login=(Button)findViewById(R.id.login_button);
 
 
         email_ip=(EditText) findViewById(R.id.email_input);
         password_ip=(EditText)findViewById(R.id.password_input);
-
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String email=email_ip.getText().toString();
                 String password=password_ip.getText().toString();
 
-                startActivity(new Intent(MainActivity.this, Signin.class));
+                startActivity(new Intent(MainActivity.this, Page2.class));
 
-                Context context = null;
-                DatabaseHelper dbHelper = new DatabaseHelper(context);
-                SQLiteDatabase db = dbHelper.getReadableDatabase();
+                DatabaseHelper dbHelper = new DatabaseHelper(MainActivity.this);
 
 
-
-                if(DatabaseHelper.checkLogin(db,email,password))
+                if(dbHelper.checkLogin(email,password))
                 {
 
                     Toast.makeText(MainActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+                    finish();
                 }
-                finish();
+
 
             }
 
         });
-
 
     }
 }
